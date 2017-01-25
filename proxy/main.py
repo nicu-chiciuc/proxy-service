c6c6921c337b2e1b60c6a2983f80f3d2dabe3680
nicu-chiciuc <nicuchiciuc@gmail.com>
2017-01-25 18:29:14 +0200

    First commit with a proxy folder (very messy and ugly)

---
+++ proxy/main.py
@@ -0,0 +1,81 @@
+import socketserver
+import socket
+import time
+
+def recv_timeout(the_socket,timeout=2):
+    #make socket non blocking
+    the_socket.setblocking(0)
+     
+    #total data partwise in an array
+    total_data=[];
+    data='';
+     
+    #beginning time
+    begin=time.time()
+    while 1:
+        #if you got some data, then break after timeout
+        if total_data and time.time()-begin > timeout:
+            break
+         
+        #if you got no data at all, wait a little longer, twice the timeout
+        elif time.time()-begin > timeout*2:
+            break
+         
+        #recv something
+        try:
+            data = the_socket.recv(8192)
+            if data:
+                total_data.append(data.decode('utf-8'))
+                #change the beginning time for measurement
+                begin=time.time()
+            else:
+                #sleep for sometime to indicate a gap
+                time.sleep(0.1)
+        except:
+            pass
+     
+    #join all parts to make final string
+    return ''.join(total_data)
+
+def getXMLData ():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect(('127.0.0.1', 8000))
+
+	string = (b'GET /api/v1/posts.xml HTTP/1.1\r\n'
+		b'Host: 127.0.0.1:8000\r\n'
+		b'\r\n')
+
+	sock.send(string)
+
+	data = recv_timeout(sock)
+
+	print(data)
+
+	return data.encode()
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(getXMLData())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
